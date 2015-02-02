a951752bf2e187d0e14d20441943f88d6be8c177
Joan Pulgar Cuevas <joapulgar@gmail.com>
2015-02-02 00:20:16 -0400

    Login (Botón Modificado)
    Falta detalle de Autenticación.

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Amsys',
+	'name'=>'AMSYS',
 	'defaultController'=>'site/index',
 	'language'=>'es',
 	'sourceLanguage'=>'en',
