6b358beee3f2bd604b5618be984a83eb6bad478d
Zephyr <ian.zephir@gmail.com>
2015-04-30 11:34:22 -0400

    :u6708: Roles
    :v: Roles :v:

--- protected/controllers/PermisoController.php
+++ protected/controllers/PermisoController.php
@@ -12,12 +12,9 @@ class PermisoController extends Controller
 	 * @return array action filters
 	 */
 	public function filters()
-	{
-		return array(
-			'accessControl', // perform access control for CRUD operations
-			'postOnly + delete', // we only allow deletion via POST request
-		);
-	}
+   {
+      return array(array('CrugeAccessControlFilter'));
+   }
 
 	/**
 	 * Specifies the access control rules.
