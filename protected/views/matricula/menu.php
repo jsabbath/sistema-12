22e0e07f4994e0ebdf016aafe64328bac5189838
zanjeer <pipohell@gmail.com>
2015-06-30 15:09:41 -0300

    cambio menus :necktie:

--- protected/views/matricula/menu.php
+++ protected/views/matricula/menu.php
@@ -126,7 +126,45 @@ if(
 		<br>
 <?php
 }
-
+ 
+if(
+	Yii::app()->user->checkAccess('administrador') OR
+    Yii::app()->user->isSuperAdmin OR 
+    Yii::app()->user->checkAccess('director') OR
+    Yii::app()->user->checkAccess('evaluador') OR
+    Yii::app()->user->checkAccess('jefe_utp') OR
+    Yii::app()->user->checkAccess('profesor')
+){ 
+?>
+	<div class="row">
+		<a class="link-negro" href="<?php echo Yii::app()->createUrl('Curso/buscar_asistencia'); ?>">
+			<div class="span4 offset1" style="background-color:  rgba(208,164,0, 0.5);  -webkit-border-radius: 25px 5px 1px 4px; /* recuerda la primera frase */ -moz-border-radius: 24px; /* si quieres todas las esquinas iguales */ border-radius: 0px 50px 50px 0px;">
+				<div class="row">
+					<div class="span1 text-center">
+							<div class="hidden-phone">
+							<?php echo TbHtml::imagePolaroid(Yii::app()->request->baseUrl."/images/iconos/attendance_list.png"); ?>
+						</div>
+					</div>
+					<div class="span3">
+						<div class="row">
+							<div class="span3">
+								<strong>Asistencia de Curso</strong>
+							</div>
+						</div>
+						<div class="row">
+							<div class="span3">
+								<p>En este item se puede ingresar la asistencia de los alumnos de un curso</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</a>
+	</div>
+	<br>
+<?php
+}
+ 
 
 ?>
 </div>		
