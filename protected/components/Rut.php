e8be426938d5826fb751eb9ca62e835c340bdd3f
Zephyr <ian.zephir@gmail.com>
2015-07-03 15:24:53 -0300

    :u6307: formateo de rut
    Se ha agregado una FUNCION GLOBAL para dar formato al rut.

---
+++ protected/components/Rut.php
@@ -0,0 +1,17 @@
+<?php
+
+//esta clase se puede llamar desde cualquier lugar
+//se llama como cualquier funcion Rut::funcion();
+class Rut extends CApplicationComponent {
+ 	
+ 	/**
+		la funcion recibe un numero tipo 225468963
+		y devuelve el rut formateado 22.546.896-3
+ 	*/
+	public static function formato($rut) {
+
+		return number_format( substr ( $rut, 0 , -1 ) , 0, "", ".") . '-' . substr ( $rut, strlen($rut) -1 , 1 );
+	}
+ 
+}
+?>
