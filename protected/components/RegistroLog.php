b4bacafdb4aad8538627f1909807fc336035a839
Zephyr <ian.zephir@gmail.com>
2015-09-03 19:20:14 -0300

    :u6307: Registro log 1.5
    Registra automaticamente o con nombre

--- protected/components/RegistroLog.php
+++ protected/components/RegistroLog.php
@@ -12,14 +12,35 @@ class RegistroLog extends CApplicationComponent {
 
 		$direccion = realpath(Yii::app()->basePath.'/../log/');
 		$usuario = Yii::app()->user->email;
-
-		var_dump($direccion);
+		$usuario_id = Yii::app()->user->id;
 
 		date_default_timezone_set('America/Santiago');
 		$fecha = Date('Y-m-d');
 		$hora = Date("H:i:s");
 
 		$registro = $usuario." - ".$hora;
+
+		if ($file = fopen($direccion.'/notas/'.$fecha.'.txt',"a")) {
+			fwrite($file,$registro.PHP_EOL);
+
+			fclose($file);
+		}else{
+			echo "No hay archivo";
+		}
+	}
+
+	//se registra ingresando el nombre
+	public static function registroNotasUsuario($nombre) {
+
+		$direccion = realpath(Yii::app()->basePath.'/../log/');
+		$usuario = Yii::app()->user->email;
+		$usuario_id = Yii::app()->user->id;
+
+		date_default_timezone_set('America/Santiago');
+		$fecha = Date('Y-m-d');
+		$hora = Date("H:i:s");
+
+		$registro = $usuario." - ".$nombre." - ".$hora;
 
 		if ($file = fopen($direccion.'/notas/'.$fecha.'.txt',"a")) {
 			fwrite($file,$registro.PHP_EOL);
@@ -39,13 +60,31 @@ class RegistroLog extends CApplicationComponent {
 		$direccion = realpath(Yii::app()->basePath.'/../log/');
 		$usuario = Yii::app()->user->email;
 
-		var_dump($direccion);
-
 		date_default_timezone_set('America/Santiago');
 		$fecha = Date('Y-m-d');
 		$hora = Date("H:i:s");
 
 		$registro = $usuario." - ".$hora;
+
+		if ($file = fopen($direccion.'/informe/'.$fecha.'.txt',"a")) {
+			fwrite($file,$registro.PHP_EOL);
+
+			fclose($file);
+		}else{
+			echo "No hay archivo";
+		}
+	}
+
+	public static function registroInformeUsuario($nombre) {
+
+		$direccion = realpath(Yii::app()->basePath.'/../log/');
+		$usuario = Yii::app()->user->email;
+
+		date_default_timezone_set('America/Santiago');
+		$fecha = Date('Y-m-d');
+		$hora = Date("H:i:s");
+
+		$registro = $usuario." - ".$nombre." - ".$hora;
 
 		if ($file = fopen($direccion.'/informe/'.$fecha.'.txt',"a")) {
 			fwrite($file,$registro.PHP_EOL);
