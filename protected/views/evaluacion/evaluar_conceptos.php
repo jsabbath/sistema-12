0cbc28f8f02beda9414a2fe74cf3a82aa22f9260
zanjeer <pipohell@gmail.com>
2015-12-22 17:00:35 -0300

    :aerial_tramway: ez optimización

--- protected/views/evaluacion/evaluar_conceptos.php
+++ protected/views/evaluacion/evaluar_conceptos.php
@@ -124,7 +124,7 @@ if (!function_exists('array_column')) {
 	echo "no tiene alumnos";
 	} else{ ?>
 
-    <?php 
+    <?php
         $informe = $lista[0]['informe'];
         $alumnos = $lista[0]['alumnos'];
         $titulo = $informe[0]['titulo'];
@@ -149,7 +149,7 @@ if (!function_exists('array_column')) {
 
 
     <div class="tabbable"> <!-- Only required for left/right tabs -->
- 
+
 
         <ul class="nav nav-tabs" style="text-align: center">
             <?php foreach ($i['areas'] as $key => $a) { ?>
@@ -169,14 +169,14 @@ if (!function_exists('array_column')) {
         </ul>
 
         <div class="tab-content" id="tab-c">
-            <?php foreach ($i['areas'] as $key => $a) { 
+            <?php foreach ($i['areas'] as $key => $a) {
                     $conce = $a['are_con'];
                 ?>
 
               <div <?php if( $key == 0 ){ ?>class="tab-pane active"<?php } else{ ?> class="tab-pane"<?php } ?> id="tab<?php echo $a['are_id']; ?>">
-                 
+
                     <?php foreach ($conce as $key => $c) { ?>
-                        
+
                         <p>  <?php echo "C".$key ." = ". $c['texto']; // conceptos descritos?></p>
 
                     <?php } ?>
@@ -187,7 +187,7 @@ if (!function_exists('array_column')) {
                             <tr>
                                 <th>nombre</th>
                                 <?php foreach ($conce as $key => $c) { ?>
-                        
+
                                     <th title="<?php echo $c['texto']; ?>">
                                         <?php echo "C".$key; ?>
 
@@ -199,7 +199,7 @@ if (!function_exists('array_column')) {
                           </thead>
 
                            <tbody>
-                                <?php  foreach ($alumnos as $key => $alum) { 
+                                <?php  foreach ($alumnos as $key => $alum) {
                                         $count = 0;
                                         $total = 0;
                                         $notas = $alum['notas_alu'];
@@ -209,36 +209,36 @@ if (!function_exists('array_column')) {
                                             }
                                             $total += 1;
                                         }
-                                       
+
                                     ?>
                                     <tr>
-                                        <td id="mat_id" hidden> <?php echo strtoupper($alum['mat_id']); ?></td> 
+                                        <td id="mat_id" hidden> <?php echo strtoupper($alum['mat_id']); ?></td>
 
                                         <td>
-                                            <p <?php if( $count < $total ){ ?>style="color: red;"<?php } ?> > 
-                                                <strong><?php echo strtoupper($alum['nombre']); ?></strong>  
+                                            <p <?php if( $count < $total ){ ?>style="color: red;"<?php } ?> >
+                                                <strong><?php echo strtoupper($alum['nombre']); ?></strong>
                                             </p>
-                                        </td>    
+                                        </td>
 
 
-                                        <?php foreach ($conce as $key => $c) { 
+                                        <?php foreach ($conce as $key => $c) {
                                             $posicion = array_search($c['con_id'], array_column($notas, 'id_con'));
                                             $not = $notas[$posicion]['eva_nota'];
                                             $eva_id = $notas[$posicion]['eva_id'];
 
                                         ?>
-                                
+
                                             <td id="<?php echo $c['con_id']; ?>" width="10%">
                                                 <!-- <?php echo $alum['mat_id'] . $c['con_id']; ?>  -->
                                                     <form name="formName" id=" <?php echo $eva_id; ?>">
                                                         <?php foreach ($escala as $key => $e) { /*echo "mat_id:".$alum['mat_id'] .",con_id:". $c['con_id'];*/?>
-                                                            <label title="<?php echo $e; ?>" class="radio"<?php if($e == $not ){ ?>style="background-color: yellow" <?php } ?>>
-                                                                <input  id="<?php echo 'mat_id:'.$alum['mat_id'] .',con_id:'. $c['con_id']; ?>" 
-                                                                        type="radio" name="optionsRadios" 
-                                                                        value="option<?php echo $key. $alum['mat_id'] . $c['con_id']; ?>" <?php if($e == $not ){ ?>checked <?php } ?> 
+                                                            <label title="<?php echo $e; ?>" class="radio"<?php if($e == $not ){ ?>style="background-color: yellow" <?php }// rgb(255, 255, 0) ?>>
+                                                                <input  id="<?php echo 'mat_id:'.$alum['mat_id'] .',con_id:'. $c['con_id']; ?>"
+                                                                        type="radio" name="optionsRadios"
+                                                                        value="option<?php echo $key. $alum['mat_id'] . $c['con_id']; ?>" <?php if($e == $not ){ ?>checked <?php } ?>
                                                                         disabled
                                                                         >
-                                                                    <?php 
+                                                                    <?php
                                                                         echo $e
 
                                                                     ?>
@@ -250,7 +250,7 @@ if (!function_exists('array_column')) {
                                         <?php } ?>
 
                                     </tr>
-                                <?php } ?> 
+                                <?php } ?>
 
                           </tbody>
                         </table>
@@ -268,15 +268,15 @@ if (!function_exists('array_column')) {
 $('table').stickyTableHeaders();
 
 $('#b_eva').on('click',function(){
-    swal({      
-            title: "Ingrese su Password!",   
+    swal({
+            title: "Ingrese su Password!",
             type: "input",
-            inputType: "password",   
-            showCancelButton: true,   
-            closeOnConfirm: false,   
-            animation: "slide-from-top" 
-        }, 
-        function(inputValue){ 
+            inputType: "password",
+            showCancelButton: true,
+            closeOnConfirm: false,
+            animation: "slide-from-top"
+        },
+        function(inputValue){
             $.ajax({
                 url: '<?php echo $this->createUrl('evaluacion/validar_edicion'); ?>',
                 type: 'POST',
@@ -284,21 +284,21 @@ $('#b_eva').on('click',function(){
                 data: { pass: inputValue, cur: <?php echo $cur; ?> },
                 success: function(response) {
                     if(!response){
-                        swal.showInputError("Ingrese datos nuevamente");     
-                        return false;   
+                        swal.showInputError("Ingrese datos nuevamente");
+                        return false;
                     }
                     if( response == 2 ){
                         swal.showInputError("usted no  tiene permisos para editar notas de este curso");
                         return false;
-                    } 
-                    swal({   
-                        title: "Correcto!",     
+                    }
+                    swal({
+                        title: "Correcto!",
                         timer: 600,
-                        type: "success",   
-                        showConfirmButton: false 
+                        type: "success",
+                        showConfirmButton: false
                     });
 
-                    $('#b_eva').hide(); 
+                    $('#b_eva').hide();
                     $('#b_guardar').show();
 
                     window.onbeforeunload = function() {
@@ -322,23 +322,30 @@ $('#b_guardar').on('click',function(){
     var tabla = document.getElementById('tab-c');
     //console.log(tabla);
     var lista = [];
-   
+
 
     $( "form" ).each(function( index ) {
-      
+
         var id_ev = $( this ).attr('id');
-      
+
         $(this).children().each(function(){
             if( $(this).children().is(':checked') ){
 
-                var notas = {
-                    eva_id: id_ev,
-                    nota: $.trim($(this).text())
+                if( $(this).css('background-color') != "rgb(255, 255, 0)" ){ // rgb(255, 255, 0) == yellow
+                    $( this ).css('background-color', "rgb(255, 255, 0)"); //  esta chekeado  se cambia a color yellow
+                    console.log("subido");
+                    var notas = {
+                        eva_id: id_ev,
+                        nota: $.trim($(this).text())
+                    }
+                    lista.push(notas);
                 }
-                lista.push(notas);    
+
+            } else{
+                $( this ).css('background-color', ""); //  se le quita el color si no  esta seleccionado
             }
 
-        }) 
+        })
     });
 
 
@@ -349,19 +356,19 @@ $('#b_guardar').on('click',function(){
             success: function(data){
                 //location.reload();
 
-                swal({   
-                        title: "Guardado!",     
+                swal({
+                        title: "Guardado!",
                         timer: 600,
-                        type: "success",   
-                        showConfirmButton: false 
+                        type: "success",
+                        showConfirmButton: false
                     });
-                
+
                 window.onbeforeunload = function() {
-                       
+
                     }
 
 
-                  
+
             }
         })
 
@@ -376,5 +383,4 @@ $('#b_guardar').on('click',function(){
 
 
  <!--  termino  de carga  -->
-<?php } ?> 
-
+<?php } ?>
